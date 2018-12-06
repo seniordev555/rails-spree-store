0d01cf7b492f63366c090f5c728a466228df55b3
seniordev555 <seniordev555@gmail.com>
2018-12-06 16:43:28 +0300

    Add SMTP settings

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,18 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { :host => 'http://localhost:3000/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV['GMAIL_USERNAME'],
+    password: ENV['GMAIL_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
