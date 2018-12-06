f675f19795e2f374acd730a86bcd38f0063c59db
seniordev555 <seniordev555@gmail.com>
2018-12-06 21:01:45 +0300

    Enable spree mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,6 +35,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.override_actionmailer_config = false
+  config.enable_mail_delivery = true
   config.action_mailer.default_url_options = { :host => 'http://localhost:3000/' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
