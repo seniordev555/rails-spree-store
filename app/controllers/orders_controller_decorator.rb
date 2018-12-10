bdec1f789fddbaeca3f7c3c7ee589cbd0175736d
seniordev555 <seniordev555@gmail.com>
2018-12-10 19:20:16 +0300

    Add links to show PDFs to the last step of checkout

---
+++ app/controllers/orders_controller_decorator.rb
@@ -0,0 +1,23 @@
+Spree::OrdersController.class_eval do
+  def show_invoice
+    @order = Spree::Order.includes(line_items: [variant: [:option_values, :images, :product]], bill_address: :state, ship_address: :state).find_by!(number: params[:id])
+
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: 'invoice.pdf', template: 'spree/orders/invoice_pdf.html.erb', layout: 'pdf.html.erb'
+      end
+    end
+  end
+
+  def show_contract
+    @order = Spree::Order.includes(line_items: [variant: [:option_values, :images, :product]], bill_address: :state, ship_address: :state).find_by!(number: params[:id])
+
+    respond_to do |format|
+      format.html
+      format.pdf do
+        render pdf: 'contract.pdf', template: 'spree/orders/contract_pdf.html.erb', layout: 'pdf.html.erb'
+      end
+    end
+  end
+end
